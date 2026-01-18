62f5af52e48f5b2312338bb46871294ab74eb0a8
dexterhere04 <sdrtharun@gmail.com>
2026-01-18 17:11:42 +0530

    changed test cases to work in a mock db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,14 +3,25 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
+  const mockPrismaService = {
+    user: {
+      findMany: jest.fn().mockResolvedValue([]),
+      create: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockPrismaService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
