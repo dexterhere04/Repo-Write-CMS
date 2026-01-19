93448187807339249382de9a6bafeac48c95ff6e
dexterhere04 <sdrtharun@gmail.com>
2026-01-19 23:54:08 +0530

    implementation of ownership of blogs

---
+++ src/auth/guards/ownersip.guard.ts
@@ -0,0 +1,47 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class OwnershipGuard implements CanActivate {
+  constructor(private prisma: PrismaService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<{
+      user?: { id: string; role: Role };
+      params: { id: string };
+    }>();
+    const user = request.user;
+    const contentId = request.params.id;
+    if (!user) {
+      throw new ForbiddenException('User not authenticated');
+    }
+
+    if (!user) {
+      throw new ForbiddenException('User not authenticated');
+    }
+
+    if (user.role == Role.ADMIN) {
+      return true;
+    }
+
+    const content = await this.prisma.client.content.findUnique({
+      where: { id: contentId },
+    });
+
+    if (!content) {
+      throw new ForbiddenException('Content not found');
+    }
+
+    if (content.authorId !== user.id) {
+      throw new ForbiddenException('You do not own this content');
+    }
+
+    return true;
+  }
+}
---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../enums/role.enum';
+
+interface AuthenticatedRequest extends Request {
+  user: {
+    role: Role;
+  };
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest<AuthenticatedRequest>();
+    const user = request.user;
+    if (!user) {
+      return false;
+    }
+    return requiredRoles.includes(user.role);
+  }
+}
