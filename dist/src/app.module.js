8f6ccb051605dd841c58560eb1427782cc2f6ef3
dexterhere04 <sdrtharun@gmail.com>
2026-01-21 12:43:47 +0530

    modules integration and adding health check

--- dist/src/app.module.js
+++ dist/src/app.module.js
@@ -11,12 +11,14 @@ const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const prisma_module_1 = require("./prisma/prisma.module");
+const auth_module_1 = require("./auth/auth.module");
+const content_module_1 = require("./content/content.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [prisma_module_1.PrismaModule],
+        imports: [prisma_module_1.PrismaModule, auth_module_1.AuthModule, content_module_1.ContentModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { AuthModule } from './auth/auth.module';
+import { ContentModule } from './content/content.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, ContentModule],
   controllers: [AppController],
   providers: [AppService],
 })
